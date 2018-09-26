3b8c871310da4552d830de9a9384f55a92ca9270
Croydon <cr0ydon@outlook.com>
2018-09-26 22:16:03 +0200

    WIP

--- conan/build.py
+++ conan/build.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
     builder.add_common_builds(pure_c=False)
     filtered_builds = []
     for settings, options, env_vars, build_requires, reference in builder.items:
-        if disable_shared == "True" and options["shared"]:
+        if disable_shared == "True" and options.shared:
              filtered_builds.append([settings, options, env_vars, build_requires])
     builder.builds = filtered_builds
     builder.run()
