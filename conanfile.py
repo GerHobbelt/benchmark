5bce54cfdc9ac6a9d32010cc6e7672153e89a6ec
Croydon <cr0ydon@outlook.com>
2018-09-29 22:29:56 +0200

    Conan: Use official env var CONAN_RUN_TESTS

--- conanfile.py
+++ conanfile.py
@@ -14,20 +14,18 @@ class GoogleBenchmarkConan(ConanFile):
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
-        "enable_exceptions": [True, False],
         "enable_lto": [True, False],
-        "enable_testing": [True, False],
+        "enable_exceptions": [True, False],
         "enable_gtest_tests": [True, False]
     }
-    default_options = "shared=False", "fPIC=True", "enable_exceptions=True", "enable_lto=False", "enable_testing=False", "enable_gtest_tests=False"
+    default_options = "shared=False", "fPIC=True", "enable_lto=False", "enable_exceptions=True", "enable_gtest_tests=False"
     exports_sources = ["*"]
     generators = "cmake"
 
     build_subfolder = "."
 
     def source(self):
-        """Wrap the original CMake file to call conan_basic_setup
-        """
+        # Wrap the original CMake file to call conan_basic_setup
         shutil.move("CMakeLists.txt", "CMakeListsOriginal.txt")
         shutil.move(os.path.join("conan", "CMakeLists.txt"), "CMakeLists.txt")
 
@@ -37,17 +35,25 @@ class GoogleBenchmarkConan(ConanFile):
                 raise Exception("{} {} does not support Visual Studio <= 12".format(self.name, self.version))
             del self.options.fPIC
 
+        if tools.get_env("CONAN_RUN_TESTS") != "True":
+            self.output.info("Environment variable CONAN_RUN_TEST is not defined.")
+            self.output.info("Tests are disabled and the option enable_gtest_tests isn't available.")
+            del self.options.enable_gtest_tests
+
     def configure(self):
         if self.settings.os == "Windows" and self.options.shared:
             raise Exception("Windows shared builds are not supported right now, see issue #639")
 
-        if self.options.enable_testing == False:
-            self.options.enable_gtest_tests = False
-
     def _configure_cmake(self):
         cmake = CMake(self)
-        cmake.definitions['BENCHMARK_ENABLE_TESTING'] = "ON" if self.options.enable_testing else "OFF"
-        cmake.definitions['BENCHMARK_ENABLE_GTEST_TESTS'] = "ON" if self.options.enable_gtest_tests and self.options.enable_testing else "OFF"
+
+        if tools.get_env("CONAN_RUN_TESTS"):
+            cmake.definitions["BENCHMARK_ENABLE_TESTING"] = "ON"
+            cmake.definitions["BENCHMARK_ENABLE_GTEST_TESTS"] = "ON" if self.options.enable_gtest_tests else "OFF"
+        else:
+            cmake.definitions["BENCHMARK_ENABLE_TESTING"] = "OFF"
+            cmake.definitions["BENCHMARK_ENABLE_GTEST_TESTS"] = "OFF"
+
         cmake.definitions["BENCHMARK_ENABLE_LTO"] = "ON" if self.options.enable_lto else "OFF"
         cmake.definitions["BENCHMARK_ENABLE_EXCEPTIONS"] = "ON" if self.options.enable_exceptions else "OFF"
 
@@ -62,7 +68,7 @@ class GoogleBenchmarkConan(ConanFile):
         return cmake
 
     def build_requirements(self):
-        if self.options.enable_gtest_tests:
+        if tools.get_env("CONAN_RUN_TESTS") and self.options.enable_gtest_tests:
             self.build_requires("gtest/1.8.0@bincrafters/stable")
 
     def build(self):
@@ -82,4 +88,11 @@ class GoogleBenchmarkConan(ConanFile):
         elif self.settings.os == "Windows":
             self.cpp_info.libs.append("shlwapi")
         elif self.settings.os == "SunOS":
-            self.cpp.info.libs.append("kstat")
+            self.cpp_info.libs.append("kstat")
+
+    def package_id(self):
+        # Development options don't change the binary output
+        # from a user point of view; overwrite id information
+        # so that the package_id is the same
+        # if only the development options are different
+        del self.info.options.enable_gtest_tests
