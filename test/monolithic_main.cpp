220b07b3bbed7d85f56dca75c1222662558fb59c
Ger Hobbelt <ger@hobbelt.com>
2022-07-06 20:56:00 +0200

    refactoring: using the new monolithic build template code for the dispatchers in every monolith.

--- test/monolithic_main.cpp
+++ test/monolithic_main.cpp
@@ -1,15 +1,4 @@
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#ifdef _MSC_VER
-#include <io.h>
-#else
-#include <unistd.h>
-#endif
-#include <errno.h>
-
 #define BUILD_MONOLITHIC 1
 #include "monolithic_examples.h"
 
@@ -23,22 +12,13 @@ extern "C" {
 }
 
 
+#include "monolithic_examples.h"
 
-typedef int tool_f();
-typedef int tool_fa(int argc, const char** argv);
+#define USAGE_NAME   "gbenchtest"
 
-static int usage();
+#include "monolithic_main_internal_defs.h"
 
-
-static struct cmd_info
-{
-	const char* cmd;
-	union
-	{
-		tool_f* f;
-		tool_fa* fa;
-	} f;
-} commands[] = {
+MONOLITHIC_CMD_TABLE_START()
 	{ "args_product", {.fa = gbenchmark_args_product_test_main } },
 	{ "basic", {.fa = gbenchmark_basic_test_main } },
 	{ "bench", {.fa = gbenchmark_bench_test_main } },
@@ -68,65 +48,6 @@ static struct cmd_info
 	{ "user_counters_thousands", {.fa = gbenchmark_user_counters_thousands_test_main } },
 
 	{ "gbenchmark", {.fa = gbenchmark_main } },
+MONOLITHIC_CMD_TABLE_END();
 
-	{ "?", {.f = usage } },
-	{ "h", {.f = usage } },
-	{ "help", {.f = usage } },
-	{ "-?", {.f = usage } },
-	{ "-h", {.f = usage } },
-	{ "--help", {.f = usage } },
-};
-
-
-static void help(void)
-{
-	fprintf(stderr, "Commands:\n");
-	for (int i = 0; i < sizeof(commands) / sizeof(commands[0]); i++)
-	{
-		struct cmd_info el = commands[i];
-		fprintf(stderr, "  %s\n", el.cmd);
-	}
-}
-
-
-static int usage()
-{
-	fprintf(stderr, "Usage: clipp_demo [command]\n");
-	help();
-	return 1;
-}
-
-
-static int parse(const char* source, int argc, const char** argv)
-{
-	if (source)
-	{
-		for (int i = 0; i < sizeof(commands) / sizeof(commands[0]); i++)
-		{
-			struct cmd_info el = commands[i];
-			size_t cmd_len = strlen(el.cmd);
-			char sentinel = 0;
-
-			if (strlen(source) >= cmd_len)
-				sentinel = source[cmd_len];
-
-			if (strncmp(source, el.cmd, cmd_len) == 0 && (sentinel == 0 || isspace(sentinel)))
-			{
-				return el.f.fa(argc, argv);
-			}
-		}
-
-		fprintf(stderr, "Unknown command '%s'.\n\nUse 'h' or 'help' command to get a list of supported commands.\n", source);
-		return 1;
-	}
-	else
-	{
-		return usage();
-	}
-}
-
-int main(int argc, const char** argv)
-{
-	return parse(argv[1], argc - 1, argv + 1);
-}
-
+#include "monolithic_main_tpl.h"
