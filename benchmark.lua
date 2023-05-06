1b1080ec17bcfc1d468cbc78ce8d702b8233c98a
Kevin Parks <kparks@esri.com>
2023-05-05 17:07:44 -0700

    Utilize defines provided by cmake build
    Ran cmake locally and pulled the defines calculated into the premake
    lua file for rtc builds.

--- benchmark.lua
+++ benchmark.lua
@@ -20,23 +20,31 @@ excludes {
   "src/benchmark_main.cc",
 }
 
-if (_PLATFORM_ANDROID) then
+if (_PLATFORM_ANDROID or _PLATFORM_IOS or _PLATFORM_WINUWP) then
+  return
 end
 
-if (_PLATFORM_COCOA) then
-end
-
-if (_PLATFORM_IOS) then
-end
+defines {
+  "BENCHMARK_STATIC_DEFINE",
+  "HAVE_STD_REGEX",
+  "HAVE_STEADY_CLOCK",
+}
 
 if (_PLATFORM_LINUX) then
+  defines {
+    "BENCHMARK_HAS_PTHREAD_AFFINITY",
+    "HAVE_POSIX_REGEX",
+    "HAVE_PTHREAD_AFFINITY",
+    "HAVE_THREAD_SAFETY_ATTRIBUTES",
+  }
 end
 
 if (_PLATFORM_MACOS) then
+  defines {
+    "HAVE_POSIX_REGEX",
+    "HAVE_THREAD_SAFETY_ATTRIBUTES",
+  }
 end
 
 if (_PLATFORM_WINDOWS) then
-end
-
-if (_PLATFORM_WINUWP) then
 end
